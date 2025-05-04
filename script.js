150afd056f4c5c2bc038f321ff9fa63ac10b6cc9
IamDebolina <144379424+IamDebolina@users.noreply.github.com>
2025-05-04 23:20:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Theme Toggle Button
+const toggleBtn = document.getElementById('themeToggle');
+const body = document.body;
+
+// Load theme preference from localStorage
+if (localStorage.getItem('theme') === 'dark') {
+  body.classList.add('dark-mode');
+  toggleBtn.textContent = '☀️';
+}
+
+// Toggle event
+toggleBtn.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+  if (body.classList.contains('dark-mode')) {
+    toggleBtn.textContent = '☀️';
+    localStorage.setItem('theme', 'dark');
+  } else {
+    toggleBtn.textContent = '🌙';
+    localStorage.setItem('theme', 'light');
+  }
+});
+
+
+  const menuToggle = document.getElementById('menuToggle');
+  const navLinks = document.getElementById('navLinks');
+
+  menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
